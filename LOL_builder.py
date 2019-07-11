cfd110e404a89b9a41f9169f2389e47e9a277207
Brendan Brzycki <bbrzycki@uchicago.edu>
2019-07-11 10:36:26 -0700

    clean up a little bit

--- LOL_builder.py
+++ LOL_builder.py
@@ -3,16 +3,23 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import requests
 
-def getPlayerStatsURL(playerName):
-    return "https://lol.gamepedia.com/Special:RunQuery/TournamentStatistics?TS%5Bpreload%5D=PlayerByChampion&TS%5Btournament%5D=LCS%202019%20Summer&TS%5Blink%5D=" + playerName + "&pfRunQueryFormName=TournamentStatistics"
+def getPlayerStatsURL(player_name):
+    return "https://lol.gamepedia.com/Special:RunQuery/TournamentStatistics?TS%5Bpreload%5D=PlayerByChampion&TS%5Btournament%5D=LCS%202019%20Summer&TS%5Blink%5D=" + player_name + "&pfRunQueryFormName=TournamentStatistics"
+
+def describePlayer(player_stats):
+```
+playerStats (df): contains fantasy points per week
+
+```
+    return
 
 def main():
-    doubleliftURL = getPlayerStatsURL("doublelift")
-    #table = pd.read_html(doubleliftURL)
+    doublelift_URL = getPlayerStatsURL("doublelift")
+    #table = pd.read_html(doublelift_URL)
 
-    playerPage = requests.get(doubleliftURL)
-    playerSoup = BeautifulSoup(playerPage.content)
-    playerStats = playerSoup.find()
+    player_page = requests.get(doublelift_URL)
+    player_soup = BeautifulSoup(player_page.content)
+    player_stats = playerSoup.find()
 
     print(soup)
 
--- lineupBuilder.py
+++ lineupBuilder.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def buildLineup(playerInfo, positionDict, cap):
+def buildLineup(player_info, position_dict, cap):
     '''
     playerInfo: Pandas DataFrame containing player name, position, value, salary
     positionDict: Dictionary with keys representing each position needed and
@@ -16,72 +16,72 @@ def buildLineup(playerInfo, positionDict, cap):
     lineup with the most efficient available player within the same position.
     '''
     lineup = []
-    netCost = 0
-    netValue = 0
+    net_cost = 0
+    net_value = 0
 
-    foundPositionDict = {}
-    cheapPlayerInfo = playerInfo.sort_values("Salary") #sort by cheapest player
+    found_position_dict = {}
+    cheap_player_info = playerInfo.sort_values("Salary") #sort by cheapest player
 
-    for i in range(len(cheapPlayerInfo)): #build basic lineup
-        pos = cheapPlayerInfo.iloc[i]["Position"]
-        salary = cheapPlayerInfo.iloc[i]["Salary"]
-        value = cheapPlayerInfo.iloc[i]["Value"]
-        name = cheapPlayerInfo.index[i]
-        if pos in positionDict: #assuming values of positionDict are POSITIVE
-            if pos not in foundPositionDict:
-                foundPositionDict[pos] = 1
-                netCost += salary
-                netValue += value
+    for i in range(len(cheap_player_info)): #build basic lineup
+        pos = cheap_player_info.iloc[i]["Position"]
+        salary = cheap_player_info.iloc[i]["Salary"]
+        value = cheap_player_info.iloc[i]["Value"]
+        name = cheap_player_info.index[i]
+        if pos in position_dict: #assuming values of positionDict are POSITIVE
+            if pos not in found_position_dict:
+                found_position_dict[pos] = 1
+                net_cost += salary
+                net_value += value
                 lineup.append(name)
-            elif foundPositionDict[pos] < positionDict[pos]:
-                foundPositionDict[pos] += 1
-                netCost += salary
-                netValue += value
+            elif found_position_dict[pos] < position_dict[pos]:
+                found_position_dict[pos] += 1
+                net_cost += salary
+                net_value += value
                 lineup.append(name)
 
-    if netCost > cap: #sanity check
+    if net_cost > cap: #sanity check
         print("Error: total cost of cheapest possible lineup exceeds salary cap.")
         return None
 
-    playerInfo["Efficiency"] = pd.to_numeric(playerInfo["Value"])/pd.to_numeric(playerInfo["Salary"])
-    efficientPlayerInfo = playerInfo.sort_values("Efficiency", ascending=False)
-    lineup.sort(key = lambda x: playerInfo.loc[x]["Efficiency"]) #sort by increasing efficiency
-    print(efficientPlayerInfo)
+    player_info["Efficiency"] = pd.to_numeric(player_info["Value"])/pd.to_numeric(player_info["Salary"])
+    efficient_player_info = player_info.sort_values("Efficiency", ascending=False)
+    lineup.sort(key = lambda x: player_info.loc[x]["Efficiency"]) #sort by increasing efficiency
+    print(efficient_player_info)
 
     for i in range(len(lineup)): #replace least efficient players first
-        pos = playerInfo.loc[lineup[i]]["Position"]
-        salary = playerInfo.loc[lineup[i]]["Salary"]
-        value = playerInfo.loc[lineup[i]]["Value"]
-        posPool = playerInfo[playerInfo["Position"] == pos].sort_values("Efficiency", ascending=False)
-        for j in range(len(posPool)):
-            name = posPool.index[j]
-            newCost = netCost - salary + playerInfo.loc[name]["Salary"]
-            newValue = netValue - value + playerInfo.loc[name]["Value"]
-            if name not in lineup and newValue > netValue and newCost <= cap: #update player
+        pos = player_info.loc[lineup[i]]["Position"]
+        salary = player_info.loc[lineup[i]]["Salary"]
+        value = player_info.loc[lineup[i]]["Value"]
+        pos_pool = player_info[player_info["Position"] == pos].sort_values("Efficiency", ascending=False)
+        for j in range(len(pos_pool)):
+            name = pos_pool.index[j]
+            new_cost = net_cost - salary + playerInfo.loc[name]["Salary"]
+            new_value = net_value - value + playerInfo.loc[name]["Value"]
+            if name not in lineup and new_value > net_value and new_cost <= cap: #update player
                 lineup[i] = name
-                netCost = newCost
-                netValue = newValue
+                net_cost = new_cost
+                net_value = new_value
 
     return lineup
 
-def describeLineup(playerInfo, lineup):
-    totalValue = 0
-    totalSalary = 0
+def describeLineup(player_info, lineup):
+    total_value = 0
+    total_salary = 0
     for name in lineup:
-        totalValue += int(playerInfo.loc[name]["Value"])
-        totalSalary += int(playerInfo.loc[name]["Salary"])
-    print("This lineup has value index {} and requires a salary of ${}.".format(totalValue, totalSalary))
+        total_value += int(player_info.loc[name]["Value"])
+        total_salary += int(player_info.loc[name]["Salary"])
+    print("This lineup has value index {} and requires a salary of ${}.".format(total_value, total_salary))
 
 def main():
-    playerInfo = pd.read_csv("playerinfo.csv")
-    playerInfo.set_index("Name", inplace=True)
+    player_info = pd.read_csv("playerinfo.csv")
+    player_info.set_index("Name", inplace=True)
 
     cap = 20000
-    positionDict = {"QB":1, "TE":1, "WR":2} #positional requirements
-    lineup = buildLineup(playerInfo, positionDict, cap)
+    position_dict = {"QB":1, "TE":1, "WR":2} #positional requirements
+    lineup = buildLineup(player_info, position_dict, cap)
 
     print(lineup, "\n")
-    describeLineup(playerInfo, lineup)
+    describeLineup(player_info, lineup)
 
 if __name__ == '__main__':
     main()
