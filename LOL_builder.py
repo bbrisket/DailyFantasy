f9b43b02616005306ed59b6044bf1fdc50f2e53e
Brendan Brzycki <bbrzycki@uchicago.edu>
2019-07-18 12:12:45 -0700

    chipping away

--- LOL_builder.py
+++ LOL_builder.py
@@ -7,12 +7,11 @@ def getPlayerStatsURL(player_name):
     return "https://lol.gamepedia.com/Special:RunQuery/TournamentStatistics?TS%5Bpreload%5D=PlayerByChampion&TS%5Btournament%5D=LCS%202019%20Summer&TS%5Blink%5D=" + player_name + "&pfRunQueryFormName=TournamentStatistics"
 
 def analyze_player(player_stats, recency_bias):
-```
-player_stats (df): contains fantasy points per week
-recency_bias (double): value describing the weight to assign to more recent performances,
-    between 0 (least) to 1 (most) 
-
-```
+    '''
+    player_stats (df): contains fantasy points per week
+    recency_bias (double): value describing the weight to assign to more recent performances,
+        between 0 (least) to 1 (most)
+    '''
     numWeeks = 1
     points = [player_stats.iloc(i)[pts] for i in range(numWeeks)]
 
@@ -28,15 +27,26 @@ recency_bias (double): value describing the weight to assign to more recent perf
 
     return avg, skewed_avg, std
 
+
+
+def build_lineup(all_stats, position_requirements, risk = 0):
+    '''
+    risk = 0 indicates that the lineup should be constructed conservatively
+    risk = 1 will attempt to build a high risk/high reward lineup
+    '''
+    for row_num in len(all_stats.index):
+        return 0
+
+
 def main():
     doublelift_URL = getPlayerStatsURL("doublelift")
     #table = pd.read_html(doublelift_URL)
 
     player_page = requests.get(doublelift_URL)
-    player_soup = BeautifulSoup(player_page.content)
-    player_stats = playerSoup.find()
+    player_soup = BeautifulSoup(player_page.content, 'html.parser')
+    player_stats = player_soup.find('div', id="mw-content-text")
 
-    print(soup)
+    print(player_stats)
 
 if __name__ == '__main__':
     main()
