7d835bacd6efdbd80755f9fea9f6cb092351f594
Brendan Brzycki <bbrzycki@uchicago.edu>
2019-07-22 13:18:52 -0700

    Migrate dataCleaner.py code over to setupDB.py

--- setupDB.py
+++ setupDB.py
@@ -1,9 +1,28 @@
+import pandas as pd
 import sqlite3
 
-conn = sqlite3.connect("dfs.db")
-c = conn.cursor()
+def main():
+    conn = sqlite3.connect("dfs.db")
+    c = conn.cursor()
 
-c.execute('''CREATE TABLE CONTESTS
-             ([Name] text, [Prize Pool] float, [Buy In] float, [Top Prize] integer,
-                [Max Entries] integer, [Entries] integer, [Cash Line] float,
-                [Winner] text, [Winning Score] float, [Week] text)''')
+    c.execute('''CREATE TABLE CONTESTS
+                ([Name] text, [Prize Pool] float, [Buy In] float, [Top Prize] integer,
+                    [Max Entries] integer, [Entries] integer, [Cash Line] float,
+                    [Winner] text, [Winning Score] float, [Week] text)''')
+
+    contest_df = pd.DataFrame()
+
+    for i in range(1, 18):
+        week = "Week " + str(i)
+        temp_df = pd.read_excel("DK_NFL_contest_data_2018.xlsx", sheet_name=week)
+        temp_df["Week"] = week
+
+    contest_df = pd.concat([df, temp_df])
+    contest_df = contest_df.dropna() #get rid of contests with missing data
+
+    #sql_contest_df = pd.to_sql()
+
+    return 0
+
+if __name__ == '__main__':
+    main()
