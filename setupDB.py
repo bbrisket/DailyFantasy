70edffc54646e2cfad4cbf356e5f2d0c6d479b5f
Brendan Brzycki <bbrzycki@uchicago.edu>
2019-07-22 16:08:10 -0700

    Beginning to replace SQLite3 with SQLAlchemy

--- setupDB.py
+++ setupDB.py
@@ -1,15 +1,9 @@
 import pandas as pd
 import sqlite3
+from sqlalchemy import create_engine
 
 def main():
-    conn = sqlite3.connect("dfs.db")
-    c = conn.cursor()
-
-    c.execute('''CREATE TABLE IF NOT EXISTS CONTESTS
-                ([Name] text, [Link] text, [Prize Pool] float, [Buy In] float,
-                    [Top Prize] integer, [Max Entries] integer, [Entries] integer,
-                    [Cash Line] float, [Winner] text, [Winning Score] float,
-                    [Week] text)''')
+    engine = create_engine("sqlite:///dfs.db")
 
     contest_df = pd.DataFrame()
 
@@ -21,7 +15,13 @@ def main():
     contest_df = pd.concat([contest_df, temp_df])
     contest_df = contest_df.dropna() #get rid of contests with missing data
 
-    contest_df.to_sql(name='CONTESTS', con=conn, if_exists='append', index = False)
+    contest_df.to_sql(name='CONTESTS', con=engine, if_exists='append', index = False)
+
+#    engine.execute('''CREATE TABLE CONTESTS
+#            ([Name] text, [Link] text, [Prize Pool] float, [Buy In] float,
+#                [Top Prize] integer, [Max Entries] integer, [Entries] integer,
+#                [Cash Line] float, [Winner] text, [Winning Score] float,
+#                [Week] text)''')
 
     return 0
 
