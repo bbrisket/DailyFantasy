2a4ea26c9582fc2b438b60a992c03430ae1b7d1a
Brendan Brzycki <bbrzycki@uchicago.edu>
2019-07-25 11:33:46 -0700

    Now scrapes PFF for player stats

--- setupDB.py
+++ setupDB.py
@@ -1,8 +1,22 @@
 import pandas as pd
+import numpy as np
 import sqlite3
+from bs4 import BeautifulSoup
+import requests
+
+#    c.execute('''CREATE TABLE CONTESTS
+#            ([Name] text, [Link] text, [Prize Pool] float, [Buy In] float,
+#                [Top Prize] integer, [Max Entries] integer, [Entries] integer,
+#                [Cash Line] float, [Winner] text, [Winning Score] float,
+#                [Week] text)''')
+
+def fetch_NFL_stats():
+    return 0
+
 
 def main():
-    #engine = create_engine("sqlite:///dfs.db")
+    ### Get contest info
+
     conn = sqlite3.connect("dfs.db")
     c = conn.cursor()
 
@@ -17,16 +31,34 @@ def main():
     contest_df = contest_df.dropna() #get rid of contests with missing data
     contest_df.to_sql(name='CONTESTS', con=conn, if_exists='replace', index = False)
 
-#    c.execute('''CREATE TABLE CONTESTS
-#            ([Name] text, [Link] text, [Prize Pool] float, [Buy In] float,
-#                [Top Prize] integer, [Max Entries] integer, [Entries] integer,
-#                [Cash Line] float, [Winner] text, [Winning Score] float,
-#                [Week] text)''')
-
-    contest_names = c.execute("SELECT Name FROM CONTESTS WHERE Name LIKE '%Double%'")
+    contest_names = c.execute("SELECT Name, [Winning Score] FROM CONTESTS WHERE Name LIKE '%Double%'")
     print(contest_names.fetchall())
 
-    return 0
+    ### Get player info
+
+    url = "https://www.pro-football-reference.com/play-index/pgl_finder.cgi?request=1&match=game&year_min=2018&year_max=2018&season_start=1&season_end=-1&age_min=0&age_max=99&game_type=A&league_id=&team_id=&opp_id=&game_num_min=0&game_num_max=99&week_num_min=1&week_num_max=1&game_day_of_week=&game_location=&game_result=&handedness=&is_active=&is_hof=&c1stat=pass_att&c1comp=gt&c1val=1&c2stat=&c2comp=gt&c2val=&c3stat=&c3comp=gt&c3val=&c4stat=&c4comp=gt&c4val=&order_by=pass_rating&from_link=1"
+    page = requests.get(url);
+    soup = BeautifulSoup(page.content, 'lxml')
+    table = soup.find('table')
+
+    rows = table.findAll('tr')
+    first_flag = True
+
+    for row in rows:
+        cols = row.findAll('td')
+
+        if not cols:
+            continue
+
+        if first_flag:
+            colnames = [col['data-stat'] for col in cols]
+            data = pd.DataFrame(columns = colnames)
+            first_flag = False
+
+        text = [col.get_text() for col in cols if col] #list of stats for each player
+        data = data.append(pd.DataFrame([text], columns = colnames))
+
+    print(data)
 
 if __name__ == '__main__':
     main()
